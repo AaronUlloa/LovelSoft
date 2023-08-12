d3e11c764190c9d68ce9f858e5068c522102b0d1
AaronUlloa <aaronmars77@gmail.com>
2023-08-11 18:21:49 -0600

    Working in Navbar

--- views/templates/header.php
+++ views/templates/header.php
@@ -1,27 +1,43 @@
 <header class="header">
-	<div class="header__container">
+	<div class="container">
 		<div class="top-bar">
-			<div class="top-bar__container">
-				<a href="/">
-					<picture class="top-bar__logo">
-						<source srcset="build/img/logo-01.avif" type="image/avif">
-						<source srcset="build/img/logo-01.webp" type="image/webp">
-						<img loading="lazy" width="133" height="17" src="build/img/logo-01.jpg" alt="Imagen DevWebcamp">
-					</picture>
-				</a>
-				<ul class="top-bar__link top-bar__navigation">
-					<li><a class="top-bar__link top-bar__btn-nav" href="/">Inicio</a></li>
-					<li><a class="top-bar__link top-bar__btn-nav" href="/">Tienda</a></li>
-					<li><a class="top-bar__link top-bar__btn-nav" href="#">Carrito</a></li>
-					<li><a class="top-bar__link top-bar__btn-nav" href="#">Nosotros</a></li>
-					<li><a class="top-bar__link top-bar__btn-nav" href="#">SUCURSALES</a></li>
-					<li><a class="top-bar__link top-bar__btn-nav" href="#">Contacto</a></li>
+			<div class="top-bar__information">
+				<div class="top-bar__data">
+					Free shipping for standard order over $100
+				</div>
+				<div>
+					<a href="#" class="top-bar__register">
+						My Account
+					</a>
+				</div>
+			</div>	
+		</div>
+
+		<div class="bar">
+			<a href="/">
+				<picture>
+					<source srcset="build/img/logo-01.avif" type="image/avif">
+					<source srcset="build/img/logo-01.webp" type="image/webp">
+					<img loading="lazy" width="133" height="17" src="build/img/logo-01.png" alt="Imagen Lovelsoft">
+				</picture>
+			</a>
+			<nav>
+				<ul class="bar__navigation">
+					<li><a class="navigation__link" href="#">Inicio</a></li>
+					<li><a class="navigation__link" href="#">Tienda</a></li>
+					<li><a class="navigation__link" href="#">Sucursales</a></li>
+					<li><a class="navigation__link" href="#">Nosotros</a></li>
+					<li><a class="navigation__link" href="#">Contacto</a></li>
 				</ul>
-				<nav class="top-bar__navigation">
-					<a class="top-bar__btn top-bar__link-nav" href="/">Logout</a>
-					<a class="top-bar__btn top-bar__link-nav" href="/login">Login</a>
-					<a class="top-bar__btn top-bar__link-nav" href="/registro">Registrar</a>
-				</nav>
+			</nav>
+			<div class="bar__elements">
+				<a href="/">
+					<i class='fa-solid fa-cart-shopping'></i>
+				</a>
+				<a href="/">
+					<i class="fa-regular fa-heart"></i>
+				</a>
+				
 			</div>
 		</div>
 	</div>
