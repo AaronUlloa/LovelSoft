72d2c115ed16687b65a901bc705ae9103912b922
AaronUlloa <aaronmars77@gmail.com>
2023-08-17 15:45:37 -0600

    Dashboard

--- views/templates/admin-sidebar.php
+++ views/templates/admin-sidebar.php
@@ -1,9 +1,33 @@
 <aside class="dashboard__sidebar">
     <nav class="dashboard__menu">
         <a href="/admin/dashboard" class="dashboard__enlace">
-            <i class="fa-solid fa-house"></i>
+            <i class="fa-solid fa-house dashboard__icon"></i>
             <span class="dashboard__menu-texto">
                 Inicio
+            </span>
+        </a>
+        <a href="/admin/usuarios" class="dashboard__enlace">
+            <i class="fa-solid fa-users dashboard__icon"></i>
+            <span class="dashboard__menu-texto">
+                Usuarios
+            </span>
+        </a>
+        <a href="/admin/clientes" class="dashboard__enlace">
+            <i class="fa-solid fa-user dashboard__icon"></i>
+            <span class="dashboard__menu-texto">
+                Clientes
+            </span>
+        </a>
+        <a href="/admin/pedidos" class="dashboard__enlace">
+            <i class="fa-solid fa-cart-shopping dashboard__icon"></i>
+            <span class="dashboard__menu-texto">
+                Pedidos
+            </span>
+        </a>
+        <a href="/admin/productos" class="dashboard__enlace">
+            <i class="fa-solid fa-box-archive dashboard__icon"></i>
+            <span class="dashboard__menu-texto">
+                Prductos
             </span>
         </a>
     </nav>
